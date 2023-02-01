000fac1b1b74ded9cbc856ccf20090f33805997f
Aleksey2710 <aleksey_2710@mail.ru>
2023-02-01 14:54:33 +0300

    initial commit

---
+++ src/main/java/pro/sky/recipesapp/controllers/FirstController.java
@@ -0,0 +1,13 @@
+package pro.sky.recipesapp.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FirstController {
+
+    @GetMapping
+    public String helloWeb() {
+        return "hello web!";
+    }
+}
